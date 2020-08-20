76af307a1e0f982c169b975b5201a0cb31258732
Evgeny <evtsyga@gmail.com>
2020-08-20 12:09:18 +0300

    add hashMap

---
+++ hashMap.js
@@ -0,0 +1,48 @@
+// hash Map
+
+let myMap = {
+  121472: [['Bob', '+13456'], ['Marla', '+443456'], ['Denis', '+753456']], // for collision demonstrate
+};
+
+function getHash(str) {
+  let hash = '';
+  for (let i = 0; i < str.length; i++) {
+    hash += str.charCodeAt(i);
+  }
+  return Number(hash) % 1e6;
+}
+
+myMap.put = function(key, value) {
+  const id = getHash(key);
+  const inner = [key, value];
+  
+  if (id in myMap) {
+    myMap[id].push(inner);
+  } else {
+    myMap[id] = [inner]; // else ??
+  }  
+};
+
+myMap.get = function(key) {
+  const id = getHash(key);
+  
+  if (id in myMap) {
+    for (let i = 0; i < myMap[id].length; i++) {
+
+      let innerKey = myMap[id][i][0];
+      let innerValue = myMap[id][i][1];
+
+      if (innerKey === key) {
+        return innerValue;
+      }
+    }
+  }
+  return null;
+};
+
+myMap.put('Arsentiy', '+11111');
+myMap.put('Evgeny', '+33333');
+
+console.log(myMap.get('Arsentiy')); // => +11111
+console.log(myMap.get('Evgeny')); // => +33333
+console.log(myMap.get('Willy')); // => null
