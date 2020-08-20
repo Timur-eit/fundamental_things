e722bd8671002478cb49226b7c430ea21da3a2cf
Evgeny <evtsyga@gmail.com>
2020-08-20 18:58:48 +0300

    update qsort

--- quick_sort.js
+++ quick_sort.js
@@ -1,32 +1,40 @@
+debugger;
+
 const unsortArr = [9, 1, 8, 2, 7, 3, 6, 4, 5];
 //                 0  1  2  3  4  5  6  7  8
 
-function qsort(arr) {  
+function qsort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
   let startIndex = 0;
   let endIndex = arr.length - 1;
 
-  let basisElemIndex = Math.floor(endIndex - startIndex) / 2 + startIndex;
+  let basisElemIndex = Math.floor((endIndex - startIndex) / 2) + startIndex;
   let less = [];
   let moreOrEqual = [];
-  
+
   const basis = arr[basisElemIndex];
-  console.log('basis ' + basis);
-  
-  for (let i = startIndex; i <= endIndex; i++) {    
+  // console.log('basis ' + basis);
+
+  for (let i = startIndex; i <= endIndex; i++) {
     if (arr[i] < basis) {
-      less.push(arr[i]);      
+      less.push(arr[i]);
     } else {
-      moreOrEqual.push(arr[i]);      
+      moreOrEqual.push(arr[i]);
     }
   }
 
-  console.log([less, moreOrEqual]);  
   const result = less.concat(moreOrEqual);
+  // console.log([less, moreOrEqual]);
 
-  if (less.length <= 1 || moreOrEqual.length <= 1) {
+  if (less.length < 2 || moreOrEqual.length < 2) {
     return result;
   }
   return qsort(less).concat(qsort(moreOrEqual));
 }
 
-console.log(qsort(unsortArr));
+console.log(qsort(unsortArr)); // returns [1, 2, 3, 4, 5, 6, 7, 9, 8]
+console.log(qsort([2, 1])); // returns [ 1, 2 ]
+console.log(qsort([2, 1, 3])); // returns [ 2, 1, 3 ]
