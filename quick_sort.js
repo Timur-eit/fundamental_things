baef717f050da019fbd40b1d731808d6299fe697
Evgeny <evtsyga@gmail.com>
2020-08-20 18:00:57 +0300

    add qsort (in process)

---
+++ quick_sort.js
@@ -0,0 +1,32 @@
+const unsortArr = [9, 1, 8, 2, 7, 3, 6, 4, 5];
+//                 0  1  2  3  4  5  6  7  8
+
+function qsort(arr) {  
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+
+  let basisElemIndex = Math.floor(endIndex - startIndex) / 2 + startIndex;
+  let less = [];
+  let moreOrEqual = [];
+  
+  const basis = arr[basisElemIndex];
+  console.log('basis ' + basis);
+  
+  for (let i = startIndex; i <= endIndex; i++) {    
+    if (arr[i] < basis) {
+      less.push(arr[i]);      
+    } else {
+      moreOrEqual.push(arr[i]);      
+    }
+  }
+
+  console.log([less, moreOrEqual]);  
+  const result = less.concat(moreOrEqual);
+
+  if (less.length <= 1 || moreOrEqual.length <= 1) {
+    return result;
+  }
+  return qsort(less).concat(qsort(moreOrEqual));
+}
+
+console.log(qsort(unsortArr));
